48277e0ff0e41ca9ce3939015cfe3fb1ec131f7f
Kyle Melik <kyle@techbykyle.com>
2026-01-01 16:23:54 -0700

    Added the uniform border for the OrgLocation component and adjusted styles a bit.

--- src/OrgLocation.jsx
+++ src/OrgLocation.jsx
@@ -1,10 +1,22 @@
 
 const OrgLocation = ({theme}) => {
 
-    return <div style={{height: 90, paddingRight: 5}}>
-        <h1 style={{}}>860 Washington Street</h1>
-        <div style={{paddingTop: 8, fontSize: 22, color: theme.palette.text.secondary}}>WellCube Science Lab</div>
-        <div style={{fontSize: 20, color: theme.palette.text.secondary}}>4th Floor</div>
+    const style = { 
+        width: 'fit-content',
+        border: '1px solid transparent',
+        borderRadius: '10px',
+        height: 'auto', 
+        padding: '10px 10px 5px 10px',
+        boxShadow: `2px 2px 2px ${theme.palette.background.shadow}`,
+        background:
+            `linear-gradient(${theme.palette.background.default}, ${theme.palette.background.default}) padding-box,
+            linear-gradient(135deg, ${theme.palette.background.paper}, #000000) border-box`,
+    }
+
+    return <div style={style}>
+        <h1 style={{fontSize: '30.5px', lineHeight: '30px'}}>860 Washington Street</h1>
+        <div style={{lineHeight: '29px', fontSize: '22px', color: theme.palette.text.secondary}}>WellCube Science Lab</div>
+        <div style={{lineHeight: '18px', fontSize: '18px', color: theme.palette.text.secondary}}>4th Floor</div>
     </div>
 }
 
